c6badbef9ae18ea530641055f1c477f390924d94
Abhishek Sinha <avishkarabhishek786@gmail.com>
2018-09-26 17:04:36 +0530

    structred code for remote local connection

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketIO = require('socket.io')
 const bodyParser = require('body-parser')
+
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+io.set('origins', '*:*');
 
 const port = process.env.PORT || 7788
 
-const app = express()
-app.io = socketIO();
-app.io.set('origins', '*:*');
-
-const server = http.createServer(app);
-app.io.attach(server);
-
-const routes = require('./routes')(app.io)
-
-app.set('view engine', 'ejs')
-
-const middleware = [
-    express.static(path.join(__dirname, 'public')),
-    bodyParser.urlencoded({
-        extended: true
-    })
-]
+const middleware = [bodyParser.urlencoded({extended: true})]
 
 app.use(middleware)
-
-app.use('/', routes)
 
 app.use((req, res, next) => {
     res.status(404).send("Page Not Found");
@@ -36,6 +19,29 @@ app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send("Page Broke!");
 });
+
+/**Socket functions */
+
+let sendTo = (socket, message) => {
+    if (typeof socket !== undefined) {
+        socket.emit("message", JSON.stringify(message));
+        console.log(JSON.stringify(message));
+    }
+}
+
+io.on('connection', (socket)=>{
+    console.log("Server connected!");
+    
+    socket.on('message', (message)=>{
+        console.log("Client: ", message);
+    })
+
+    socket.on('close', ()=>{
+        console.log("Server closed");
+    })
+
+    sendTo(socket, `{"Server: ":"Hello from Server!"}`)
+})
 
 server.listen(port, () => {
     console.log("Application is running on port " + port);
--- routes.js
+++ routes.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-let sendTo = (socket, message) => {
-    if (typeof socket !== undefined) {
-        socket.emit("message", JSON.stringify(message));
-        console.log(JSON.stringify(message));
-    }
-}
+// let sendTo = (socket, message) => {
+//     if (typeof socket !== undefined) {
+//         socket.emit("message", JSON.stringify(message));
+//         console.log(JSON.stringify(message));
+//     }
+// }
 
-router.get('/', (req, res)=>{
-    res.render('index.ejs', {
-        data: {},
-        errors: {},
-        title: 'BTC FLO Exchange API'
-    })
-})
+// router.get('/', (req, res)=>{
+//     res.render('index.ejs', {
+//         data: {},
+//         errors: {},
+//         title: 'BTC FLO Exchange API'
+//     })
+// })
 
-module.exports = (io) => {
-    io.on('connection', (socket)=>{
-        console.log("Server connected!");
+// module.exports = (io) => {
+//     io.on('connection', (socket)=>{
+//         console.log("Server connected!");
         
-        socket.on('message', (message)=>{
-            console.log("Client: ", message);
-        })
+//         socket.on('message', (message)=>{
+//             console.log("Client: ", message);
+//         })
 
-        socket.on('close', ()=>{
-            console.log("Server closed");
-        })
+//         socket.on('close', ()=>{
+//             console.log("Server closed");
+//         })
 
-        sendTo(socket, `{"Server: ":"Hello from Server!"}`)
-    })
-    return router
-}
+//         sendTo(socket, `{"Server: ":"Hello from Server!"}`)
+//     })
+//     return router
+// }
