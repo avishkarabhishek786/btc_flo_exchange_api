fa299e565408b8161b263e281949ca5d42502f11
Abhishek Sinha <avishkarabhishek786@gmail.com>
2018-09-27 19:56:14 +0530

    local and remote exchange of messages being transaferred successfully.

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ io.set('origins', '*:*');
 
 const port = process.env.PORT || 7788
 
+const routes = require('./routes')(io)
+
 const middleware = [bodyParser.urlencoded({extended: true})]
 
 app.use(middleware)
+
+app.use('/', routes)
 
 app.use((req, res, next) => {
     res.status(404).send("Page Not Found");
@@ -19,29 +23,6 @@ app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send("Page Broke!");
 });
-
-/**Socket functions */
-
-let sendTo = (socket, message) => {
-    if (typeof socket !== undefined) {
-        socket.emit("message", JSON.stringify(message));
-        console.log(JSON.stringify(message));
-    }
-}
-
-io.on('connection', (socket)=>{
-    console.log("Server connected!");
-    
-    socket.on('message', (message)=>{
-        console.log("Client: ", message);
-    })
-
-    socket.on('close', ()=>{
-        console.log("Server closed");
-    })
-
-    sendTo(socket, `{"Server: ":"Hello from Server!"}`)
-})
 
 server.listen(port, () => {
     console.log("Application is running on port " + port);
--- routes.js
+++ routes.js
@@ -1,34 +1,68 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
+var crypto = require('crypto');
 
-// let sendTo = (socket, message) => {
-//     if (typeof socket !== undefined) {
-//         socket.emit("message", JSON.stringify(message));
-//         console.log(JSON.stringify(message));
-//     }
-// }
+let sendTo = (socket, message) => {
+    if (typeof socket !== undefined) {
+        socket.emit("message", JSON.stringify(message));
+        console.log(JSON.stringify(message));
+    }
+}
 
-// router.get('/', (req, res)=>{
-//     res.render('index.ejs', {
-//         data: {},
-//         errors: {},
-//         title: 'BTC FLO Exchange API'
-//     })
-// })
+let hash = (data) => {
+  return crypto.createHash('sha256').update(data).digest('hex');
+}
 
-// module.exports = (io) => {
-//     io.on('connection', (socket)=>{
-//         console.log("Server connected!");
+module.exports = (io) => {
+    io.on('connection', (socket)=>{
+        console.log("Server connected!");
         
-//         socket.on('message', (message)=>{
-//             console.log("Client: ", message);
-//         })
+        socket.on('message', (message)=>{
+            
+            let msg;
 
-//         socket.on('close', ()=>{
-//             console.log("Server closed");
-//         })
+            try {
+                msg = JSON.parse(message);
+            } catch (error) {
+                console.error("Failed to parse object.");
+                return
+            }
 
-//         sendTo(socket, `{"Server: ":"Hello from Server!"}`)
-//     })
-//     return router
-// }
+            if (typeof msg.data === undefined || msg.type === undefined) {
+                return console.error("Response is not a valid object.");
+            }
+
+            switch (msg.type) {
+                case "trade":
+                    trade(msg);
+                    break;
+            
+                default:
+                    break;
+            }
+        })
+
+        socket.on('close', ()=>{
+            console.log("Server closed");
+        })
+
+        let trade = (response) => {
+            if (response.type === undefined || response.type != "trade") {
+                return console.error("Response is not a valid object.");
+            }
+
+            let res_hash = hash(JSON.stringify(response.data));
+ 
+            let server_res = {
+                "type": "trade",
+                "data": null
+            }
+
+            if (response.sha256 === res_hash) {
+                  server_res.data = `Hash received: ${res_hash}`;
+            }
+            sendTo(socket, {"Server: ": JSON.stringify(server_res)});
+        }
+    })
+    return router
+}
